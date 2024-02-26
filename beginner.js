3b30362f1d245d3ad25dc3bcfa866cf6aec600f3
kdcontent <kdcontent18@gmail.com>
2024-02-26 14:48:57 +0900

    JS basics with supersimpledev

---
+++ beginner.js
@@ -0,0 +1,19 @@
+// JS basics: first challenge
+var tShirts = 10;
+var socks = 8;
+var dinnerPlates = 20;
+
+var totalCost = tShirts + socks + dinnerPlates;
+
+document.body.innerHTML = 'Total cost of the things is ' + totalCost + '$!';
+
+// Second challenge
+
+var bankAccount = 100;
+var lunch = 20;
+var dinner = 50;
+var job = 200;
+
+var moneyLeft = bankAccount - lunch - dinner + job;
+
+document.body.innerHTML = 'Total money you have is ' + moneyLeft + '$!';
