ec70d72f3ce4e0f2375f52ca6c610d72662b1155
kdcontent <kdcontent18@gmail.com>
2024-02-28 14:05:55 +0900

    Math and numbers

---
+++ numbers and math.js
@@ -0,0 +1,10 @@
+var notebook = 2.1; // -> first variable
+var book = 3.2; // -> second variable
+var pen = 1.1; // -> third variable
+
+
+var total = (notebook * 2 ) + (book * 2) + (pen * 2); // -> total cost of three items/variable
+
+console.log(total); // getting outcome in console
+
+document.write("Total cost of purchase is " + total + "$!"); // -> writing total outcome to HTML file.
