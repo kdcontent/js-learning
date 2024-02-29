3268fc27d8db6336a5e7d5c6251d41589f9fa8e3
kdcontent <kdcontent18@gmail.com>
2024-02-29 15:16:30 +0900

    Boolean exercises

--- booleans.js
+++ booleans.js
@@ -19,3 +19,68 @@ if (age > drivingAge) {
 } else {
   document.write("You are not old enough to drive or posses a car!");
 }
+// Hour of the day
+
+let hour = 10;
+
+const userName = 'Abdulboriy';
+
+if (hour >= 6 && hour < 13 ) {
+  console.log('Good morning, ' + userName + '!');
+  alert('Good morning, ' + userName + '!');
+} else if ( hour >=13 && hour < 18) {
+  console.log('Good afternoon, ' + userName + '!');
+  alert('Good afternoon, ' + userName + '!');
+} else {
+  console.log('Good night,' + userName + '!');
+  alert('Good night, ' + userName + '!');
+}
+
+
+// Discount age checker with '||' logical operator
+
+let discountAge = 4;
+const isHoliday = false;
+
+if (discountAge > 65 || discountAge < 6 && !isHoliday) {
+  console.log('You get 50% discount!');
+  alert('You get 50% discount');
+} else {
+  console.log('You do not get any discount');
+  alert('You do not get any discount!');
+}
+
+
+// Flipping card
+
+let randomCoin = Math.random();
+let result = '';
+let guess = 'Heads';
+
+
+
+
+if (randomCoin < 0.5) {
+  result = 'Heads';
+} else {
+  result = 'Tails';
+}
+if (guess === result) {
+  result = 'You win!';
+} else {
+  result = 'You lost!';
+
+}
+document.write(result);
+
+
+//ternary operator version of flipping card
+
+let randomCoin = Math.random();
+let guess = 'Heads';
+
+let result = randomCoin < 0.5 ? 'Heads' : 'Tails';
+
+result = guess === result ? 'You win!' : 'You lost!';
+
+document.write(result);
