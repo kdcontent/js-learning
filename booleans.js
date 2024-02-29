877686982c612ee983605c57518f3f060370cd66
kdcontent <kdcontent18@gmail.com>
2024-02-29 14:02:33 +0900

    Boolean

---
+++ booleans.js
@@ -0,0 +1,21 @@
+// Booleans and If statements
+
+// true -> first boolean value
+// false -> second boolean value
+// == -> converts values to the same value\
+// ===
+// && -> logical operator: 'and'
+// || -> logical operator: 'or'
+// ! -> logical operator: 'not'
+
+// Practical example of boolean and if statements
+
+let age = 10;
+let drivingAge = 18;
+
+
+if (age > drivingAge) {
+  document.write("You are old enough to drive or posses a car!");
+} else {
+  document.write("You are not old enough to drive or posses a car!");
+}
