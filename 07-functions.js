01fb62c8fb11cfca230a47b51cabdc000aaffeb4
kdcontent <kdcontent18@gmail.com>
2024-03-06 15:08:53 +0900

    Exercises on functions

--- 07-functions.js
+++ 07-functions.js
@@ -7,3 +7,39 @@ function taxCalculate(myParameter) {
   console.log(myParameter * 0.1);
 }
 taxCalculate(10220);
+
+// Greet function
+function greet(myName) {
+  if (myName === undefined) {
+    console.log('Hi there');
+  } else {
+  console.log(`Hello ${myName}`);
+}
+}
+greet();
+
+
+// convertToFarenheit
+function convertToFarenheit(celcius) {
+  console.log(`Farenheit = ${(celcius * 9/5) + 32}`);
+}
+convertToFarenheit(30);
+
+// converToCelcius
+
+function converToCelcius(Farenheit) {
+  console.log(`Celcius = ${(Farenheit - 32) * 5/9}`);
+}
+converToCelcius(90);
+
+// convertTemperature
+function convertTemperature(degrees, unit) {
+  if (unit === 'C') {
+    console.log(`Farenheit = ${(degrees * 9/5) + 32} ${unit}`);
+  } else if (unit === 'F') {
+    console.log(`Celcius = ${(degrees - 32) * 5/9} ${unit}`);
+  }
+  return console.log(200);
+}
+convertTemperature(25, 'C');
+convertTemperature(93, 'F');
