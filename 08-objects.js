89cab2fdeedbeb313d51ae6272d60c59db88c50c
kdcontent <kdcontent18@gmail.com>
2024-03-08 15:04:33 +0900

    Object

--- 08-objects.js
+++ 08-objects.js
@@ -1,45 +1,65 @@
-// Why do we use object? We can reorganize well our code and instead of writing bunch of variables, we can write object.
-
-
-// const product ={
-//  userName: 'socks',
-//  price: 1090
-// };
-// console.log(product);
-// console.log(product.price);
-// console.log(product.userName);
-
-// product.userName = 'cotton socks';
-// console.log(product);
-
-
-// product.newProperty = 'what\'s pros and cons of this next check?'
-// console.log(product);
-
-// delete product.newProperty;
-// console.log(product);
-
-
-const product2 = {
-  name: 'shirt',
-  stats: {
-    view:'4.5 mln',
-    download: '1.2 mln',
-    like: '2.8 mln'
-  },
-  birch: function myFunction() {
-    console.log('Take all the designers bullshit, what do you have?');
-  },
-
+let soccerPlayer0 = {
+name: 'Messi',
+club: 'Inter Miami',
+age: 36,
+shirtNumber: 10,
+stats: {
+  goals: 5,
+  assists: 8
+},
+fun: function myFunction() {
+console.log('Lionel Messi');
+}
 };
 
-console.log(product2.stats.like);
-// console.log(product2.birch.myFunction); -> error
-product2.birch();
+let soccerPlayer1 = {
+  name: 'Ronaldo',
+  club: 'An Nasr',
+  age: 38,
+  shirtNumber: 7,
+  stats: {
+    goals: 20,
+    assists: 3
+  },
+  fun: function myFunction1() {
+    console.log('Cristiano Ronaldo');
+  }
+};
 
-console.log(JSON.stringify(product2));
+let soccerPlayer3 = {
+  name: 'Neymar',
+  club: 'Al Hilol',
+  age: 34,
+  shirtNumber: 10,
+  stats: {
+    goals: 4,
+    assists: 2
+  },
+  fun: function myFunction2() {
+    console.log('Neymar JR');
+  }
+}
+
+soccerPlayer1.club = 'Manchester United'; // -> we can property outside of object
+soccerPlayer0.ballondors = 8;
+soccerPlayer1.ballondors = 5;
+delete soccerPlayer0.ballondors; // we can delete property from outside of object ->
+
+
+console.log(soccerPlayer0.name);
+console.log(soccerPlayer1.name);
+console.log(soccerPlayer1.club);
+console.log(soccerPlayer0);
+console.log(soccerPlayer1);
+console.log(soccerPlayer3['name']);
+console.log(soccerPlayer1.name, soccerPlayer1.stats);
+soccerPlayer0.fun();
+soccerPlayer1.fun();
+soccerPlayer3.fun();
+
+console.log(JSON.stringify(soccerPlayer0));
 
 
 
-const jsonString = JSON.stringify(product2);
-console.log(JSON.parse(jsonString));
+// Instead of creating bunch of variables, we can add all of them to one object and use it where we want;
+// we [] notation instead of '.' notation when we can't access some properties with dot notation. For examples soccerPlayer.transfer-price;
