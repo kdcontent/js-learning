1d447625daec3519120327f766df01a23e95d9f5
kdcontent <kdcontent18@gmail.com>
2024-03-21 00:41:33 +0900

    DOM

---
+++ 09-dom.js
@@ -0,0 +1,19 @@
+/*
+console.log(document.title);
+document.title ='Hello new World!';
+
+console.log(document.body);
+console.log(typeof document.body);
+
+console.log(document.body.innerHTML);
+
+document.body.innerHTML = "<a href='https://www.youtube.com/' target=_blank>Link</a>";
+
+*/
+console.log(document.querySelector('button'));
+document.querySelector('button').innerHTML = 'Changes';
+
+
+// console.log(document.querySelector('.js-button'));
+let buttonElement = document.querySelector('.js-button');
+console.log(buttonElement);
