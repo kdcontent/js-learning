3de6e299139be5fad01bad22f5ca8e4b9b115a06
kdcontent <kdcontent18@gmail.com>
2024-04-04 13:39:15 +0900

    Arrays and loops

--- 11-arrays-loops.js
+++ 11-arrays-loops.js
@@ -1,79 +1,146 @@
-let myArray = [7, 12, 15];
-console.log(myArray);
-
-myArray[1];
-console.log(myArray[1]);
-
-myArray[2] = 88;
-console.log(myArray);
-
-let myArrayZero =[1, {soccerName: 'Leo'}];
-console.log(Array.isArray(myArrayZero));
+let iArray = [10, 12, 14];
+console.log(iArray);
+console.log(iArray[2]);
 
 
-myArrayZero.push(false);
-console.log(myArrayZero);
+let mixedArray = [1, 'hello new world', true, myObject = {
+  objectName: 'kendrick',
+  objectYear: '1987'
+}]
 
-myArrayZero.splice(0, 2);
-console.log(myArrayZero);
+let arrayZero = [12, 2, 22, 'Hip-Hop', 'Rapping', {coffeeName: 'americano', coffeePrice: '3$'} ]
+console.log(mixedArray[3]);
+
+let arrayOne = ['Tesla', 'Rolls Royce', 'MB', 'BMW']
+
+console.log(Array.isArray([1, 20, 40]));
+
+console.log(iArray.length);
+console.log(mixedArray.length);
+
+mixedArray.push({objectJob: 'rapper'});
+console.log(mixedArray);
+
+arrayZero.push('One hundred zero');
+console.log(arrayZero);
+
+arrayOne.push('KIA');
+console.log(arrayOne);
+
+arrayOne.splice(1, 3);
+console.log(arrayOne);
 
 
-// Loops
+// loops
+let x = 2;
 
-let m = 2;
-// While
-while (m < 8) {
-
-console.log(m);
-m = m + 1;
-
+while (x < 10) {
+  document.body.innerHTML = x;
+  x ++;
 }
 
-// for
-for (let zero = 0; zero < 10; zero++) {
-  let soccerPlaye0 = {
-    soccerName: 'Leo',
-    soccerNumber: 10,
-    soccerClub: 'Inter Miami'
-  }
-  console.log(soccerPlaye0);
+let y = 3;
+while(y < 15) {
+  console.log('This code is looping!');
+  y += 3;
 }
 
-
-let randomNum = 0;
-
-while (randomNum < 0.5) {
-  randomNum = Math.random();
-  console.log(randomNum);
+let q = 5;
+while(q < 10) {
+  console.log('Looping!');
+  q += 2;
 }
 
-// looping through array
-const todoList = [
-  'make dinner',
-  'wash dishes',
-  'watch YouTube'
-];
-
-for (let index = 0; index <= todoList.length -1; index++) {
-let value = todoList[index];
-console.log(value);
+let z = 2;
+while(z < 20) {
+  console.log('Looping');
+  z += 2;
 }
 
-// Calculate array
-const array = [2, 8, 9, 0, 10, 33];
+for (xy = 12; xy < 64; xy += 5) {
+  console.log('For looping!')
+}
+
+const todoList = ['play soccer', 'make cash', 'watch YouTube', 'listen to music'];
+
+for (i = 0; i < todoList.length; i++) {
+  const value = todoList[i];
+  console.log(value);
+}
+
+// calculate values of array
+const nums = [2001, 2121, 2354];
 let total = 0;
-for (i = 0; i < array.length -1; i++) {
-  total += array[i];
-  console.log(total);
+for (let xz = 0; xz < nums.length; xz++) {
+  const number = nums[xz];
+  total += number;
+
 }
+console.log(total);
 
-// calculate array
+// doubled
+const doubledNumber = [];
 
-const arrayZero = [20, 221, 2113, 22, 90, 102, 2043];
-
-let sum = 0;
-for (x = 0; x < arrayZero.length; x++) {
-  sum += arrayZero[x];
-  document.body.innerHTML = sum;
-  // document.body.innerHTML = sum;
+for (b = 0; b < nums.length; b++) {
+  const number = nums[b];
+  doubledNumber.push(number * 2);
 }
+console.log(doubledNumber);
+
+const string = ['Hi ', 'my name is ', 'Slim shady'];
+let totalResult = '';
+
+for (t = 0; t < string.length; t++) {
+  const text = string[t];
+  totalResult += text;
+}
+console.log(totalResult);
+
+
+const age = ['I ', 'am ', 10, ' years old!'];
+let ageResult = '';
+
+for (a = 0; a < age.length; a++) {
+  const ageText = age[a];
+  ageResult += ageText;
+}
+console.log(ageResult);
+
+// slice
+
+arrayZero = [1, 2, 3];
+arrayOne = arrayZero.slice();
+
+arrayOne.push('hello new world');
+console.log(arrayZero);
+console.log(arrayOne);
+
+// destructuring
+
+const [firstValue, secondValue] = ['hello', 'new', 'world', '!'];
+console.log(firstValue);
+
+// Loops: break and continue
+for (zx = 0; zx <= 10; zx++) {
+   if (zx % 3 === 0) {
+    continue;
+   }
+   console.log(zx);
+  //  if (zx === 9) {
+  //   break;
+  //  }
+}
+function doubleNum(num) {
+const doubledNum = [];
+
+for (b = 0; b < num.length; b++) {
+  const number = num[b];
+  if (number === 0) {
+    return doubledNum;
+  }
+
+  doubledNum.push(number * 2);
+}
+return doubledNum;
+}
+console.log(doubleNum([12, 15, 0, 17]));
